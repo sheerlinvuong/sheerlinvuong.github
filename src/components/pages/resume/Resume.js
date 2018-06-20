72108e5689de4a011efacd5493b15ab6bb018a21
sheerlinvuong <sheerlinvuong@gmail.com>
2018-06-20 13:41:59 +0100

    c

--- src/components/pages/resume/Resume.js
+++ src/components/pages/resume/Resume.js
@@ -10,49 +10,7 @@ class Resume extends Component {
           <h1>Resume</h1>
         </header>
         <body className="CV-body">
-          <p>
-            cv will be here, maybe add pdf download Personal Profile I am a
-            diligent and open-minded individual with strong work ethics and have
-            shown myself to work effectively and responsibly within a team. I
-            aim to combine experience, initiative, and enthusiasm in my
-            profession and everything I do. Education 2014 – 2017: Swansea
-            University Electrical and Electronic Engineering (BEng): First Class
-            Honours (1st) 2012 – 2014: Lambeth Academy Sixth Form A-levels:
-            Mathematics (B), Chemistry (C), Physics (D) 2007 – 2012: Ashcroft
-            Technology Academy GCSEs: 9 A* - C including Mathematics (A),
-            Science (B), French (B), Business studies (B) BTEC: Engineering
-            (Merit Award) Work Experience October 2016 – Catering Assistant at
-            Liberty Stadium Efficiently serving customers in a fast-paced
-            environment Providing great customer service and quickly dealing
-            with problems Handling cash and operating tills Maintaining the shop
-            floor through cleaning and replenishing stock October 2015 – April
-            2017: Student Ambassador at Swansea University Representing the
-            university on open/visit days and ensuring a lively and positive
-            experience Tour guide for prospective students and parents
-            Responsible for answering any questions and providing information
-            August 2010-May 2012: Volunteer at Cancer Research Charity Shop
-            Fundraiser: Planned new and interesting ideas to raise money within
-            a team as well as organizing fundraising events on the high street
-            Sales Assistant: Provided well-mannered customer service Managing,
-            stocking and cleaning the shop floor Handing the cash machine and
-            counting the profit at the end of the day Administrative skill when
-            putting customer information into the subscription database August
-            2010: Larkhall Children’s Centre Attended to the needs of children
-            under 5 Learned about health and safety in the kitchen when
-            preparing lunch for children Looking out for hazards when organizing
-            and cleaning the play areas Skills and Qualities Python Basic HTML,
-            CSS and C Microsoft Excel Can speak fluent Cantonese Intermediate
-            Mandarin – I had the opportunity to study Mandarin as well as
-            partaking in cultural activities in China at Weihai University.
-            Additional Information Prefect at Ashcroft Technology Academy -
-            Maintaining the disciplinary standards within the academy and
-            developing communication skills by helping younger students to read.
-            Achieved Grade 5 piano, as well as tutoring children how to play
-            piano. – Demonstrates patience, persistence, and determination when
-            challenged with more difficult pieces. W Renwick Prize for the Best
-            Software-based Project by Swansea College of Engineering for my
-            third year project.
-          </p>
+          <p>cv will be here, maybe add pdf download</p>
         </body>
       </div>
     );
