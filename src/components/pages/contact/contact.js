cc7b9a7092671056bfd0ad137420a860bdeb7ae2
sheerlinvuong <sheerlinvuong@gmail.com>
2018-07-26 14:28:43 +0100

    updated contact page with reCaptcha

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -1,30 +1,107 @@
 import React, { Component } from 'react';
 import './contact.css';
+import ReCaptcha from 'react-google-recaptcha';
 
 class Contact extends Component {
+  state = {
+    form: {
+      name: '',
+      email: '',
+      message: '',
+    },
+
+    gToken: '',
+  };
+  handleChange = event => {
+    console.log(event.target.name);
+    console.log(event.target.value);
+    this.setState({
+      form: {
+        ...this.state.form,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  handleRecaptcha = value => {
+    console.log(value);
+    this.setState({
+      gToken: value,
+    });
+  };
+
   handleSubmit = event => {
     event.preventDefault();
-    console.log({ target: event.target });
-    console.log(event.target[0].value);
-    console.log(event.target.elements.username.value);
-    console.log(this.inputNode.value);
+    const { name, email, message } = this.state.form;
+    const { gToken } = this.state;
+
+    const isInvalidForm = name === '' || email === '' || message === '';
+
+    const isIncompleteRecaptcha = gToken === '';
+
+    if (isInvalidForm) {
+      alert('hey your form is incomplete, please answer all boxes');
+      return;
+    }
+
+    if (isIncompleteRecaptcha) {
+      alert('please tick recaptcha');
+      return;
+    }
+
+    const url = 'http://localhost:8080/contact';
+    fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({ ...this.state.form, gToken }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .catch(error => console.log(error))
+      .then(response => console.log(response));
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="username"
-            ref={node => {
-              this.inputNode = node;
-            }}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.form.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            email:
+            <input
+              type="text"
+              name="email"
+              value={this.state.form.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            message:
+            <input
+              type="text"
+              name="message"
+              value={this.state.form.message}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <ReCaptcha
+          ref="recaptcha"
+          sitekey="6LdeiGYUAAAAAIAihGaRFl-FZBLqRXf8DhC7lu9h"
+          onChange={this.handleRecaptcha}
+        />
+      </div>
     );
   }
 }
