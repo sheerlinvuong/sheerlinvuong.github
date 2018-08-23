417a046ef159072d079b624f61805e41da9d7157
sheerlinvuong <sheerlinvuong@gmail.com>
2018-08-23 16:41:41 +0100

    Added descriptions for projects

--- src/components/pages/projects/projects.js
+++ src/components/pages/projects/projects.js
@@ -12,10 +12,26 @@ class Projects extends Component {
           </header>
           <img src={WebformGif} width="700" />
           <body className="Proj-body">
-            <p>Creating a simple messaging bot with Node.js</p>
-            <a href="https://medium.com/@sheerlinvuong/hook-your-web-contact-form-to-telegram-f0c4ad442bfa">
+            <p>
+              A simple messaging bot with Node.js.
+              <br /> Instead of receiving contact form messages in my e-mail, I
+              used a telegram bot to relay them to me.
+              <br />
+              To achieve this I used Express.js and node-fetch to listen to HTTP
+              POST requests from the web form. The bot then posts the messages
+              to my telegram. <br /> I keep the express app running 24/7 using
+              Heroku so I can receive messages at any time.
+            </p>
+            <a
+              href="https://medium.com/@sheerlinvuong/hook-your-web-contact-form-to-telegram-f0c4ad442bfa"
+              style={{
+                textDecoration: 'none',
+                color: 'black',
+              }}
+            >
               {' '}
-              Read the full article here!
+              Want one for your website? Click here to read the tutorial I wrote
+              on Medium.
             </a>
           </body>
         </div>
@@ -25,7 +41,12 @@ class Projects extends Component {
             <h1>WhoDat App</h1>
           </header>
           <body className="Proj-body">
-            <p>The app will be able to recognise celebrities.</p>
+            <p>
+              Easily recognise celebrities with WhoDat mobile app. <br />
+              Point the camera at a celebrity and their name and the percent
+              accuracy appears. <br />I contributed to the design of the app as
+              well as implementing the gallery screen. <br />
+            </p>
           </body>
         </div>
       </div>
