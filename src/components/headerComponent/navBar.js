a0c19d0ea6f298d26f661478b708d563f4bed572
sheerlinvuong <sheerlinvuong@gmail.com>
2018-09-04 15:39:36 +0100

    CSS Redesign

--- src/components/headerComponent/navBar.js
+++ src/components/headerComponent/navBar.js
@@ -1,69 +1,68 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from './nameLogo.png';
 import './navBar.css';
 
 class NavBar extends Component {
   render() {
     return (
       <div className="App">
-        <picture className="App-logo">
-          <img src={logo} alt="logo" />
-        </picture>
-        <ul className="nav">
-          <li>
-            <NavLink
-              exact
-              to=""
-              style={{
-                textDecoration: 'none',
-                color: 'grey',
-              }}
-              activeStyle={{ color: 'lightsalmon' }}
-            >
-              ABOUT
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              exact
-              to="/projects"
-              style={{
-                textDecoration: 'none',
-                color: 'grey',
-              }}
-              activeStyle={{ color: 'lightsalmon' }}
-            >
-              PROJECTS
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              exact
-              to="/resume"
-              style={{
-                textDecoration: 'none',
-                color: 'grey',
-              }}
-              activeStyle={{ color: 'lightsalmon' }}
-            >
-              RESUME
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              exact
-              to="/contact"
-              style={{
-                textDecoration: 'none',
-                color: 'grey',
-              }}
-              activeStyle={{ color: 'lightsalmon' }}
-            >
-              CONTACT
-            </NavLink>
-          </li>
-        </ul>
+        <div className="App-logo">S H E E R L I N _ V </div>
+        <div className="nav">
+          <ul>
+            <li>
+              <NavLink
+                exact
+                to=""
+                style={{
+                  textDecoration: 'none',
+                  color: 'grey',
+                }}
+                activeStyle={{ color: 'red' }}
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                exact
+                to="/projects"
+                style={{
+                  textDecoration: 'none',
+                  color: 'grey',
+                }}
+                activeStyle={{ color: 'lightsalmon' }}
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                exact
+                to="/resume"
+                style={{
+                  textDecoration: 'none',
+                  color: 'grey',
+                }}
+                activeStyle={{ color: '	#00A86B' }}
+              >
+                Resume
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                exact
+                to="/contact"
+                style={{
+                  textDecoration: 'none',
+                  color: 'grey',
+                }}
+                activeStyle={{ color: '#007fa1' }}
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
